c71787075bb356b5a83718721dd3c2718795c868
khairulbasar02 <iamkhokon02@gmail.com>
2025-01-13 16:16:55 +0600

    new update

--- Array/MaxSubArraySum.java
+++ Array/MaxSubArraySum.java
@@ -49,6 +49,34 @@ public class MaxSubArraySum {
         System.out.println("max Sub arr sum:  " + maxSum);
     }
 
+    // max sub array sum
+    // Method 2: prefix Approach
+    // TC: O(n2)
+    // SC: O(n)
+    public static void maxSubArrSum3(int arr[]) {
+        int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+
+        // prefix: i==0 (all sub array sum calc)
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        int maxSum = Integer.MIN_VALUE; // - infinity
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+
+                int sum = (i == 0) ? prefix[j] : prefix[j] - prefix[i - 1];
+
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+            }
+        }
+        System.out.println("max Sub arr sum:  " + maxSum);
+    }
+
     public static void main(String[] args) {
         // print max sub arr sum
         int arr[] = { 2, 4, 6, 8, 10 }; // ans: 30
